5c11a1f31d4f12655456ff2079d491acf6c59d74
thenuwah <162116832+thenuwah@users.noreply.github.com>
2024-12-05 20:43:58 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "zmRzRDzb#xi4lLyEZ1DAmnl3z58X16TvmmWkMn3ulRzmM-xgSxPM",
+SESSION_ID: process.env.SESSION_ID || "nINVmTpK#yWyVWdF8SIc3lbISWYmydG6EyQZg_2dH7W97BWog-Vw",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || ""
 };
