cdf3e0d40238110d6bd83ad20a7f885ab8ee37cc
thenuwah <162116832+thenuwah@users.noreply.github.com>
2024-12-05 21:12:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
 
-const ownerNumber = ['+94729116234']
+const ownerNumber = ['94729116234']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/session/creds.json')) {
