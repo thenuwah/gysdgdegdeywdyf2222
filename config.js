39c19a2f8deb74547f62e45748b2785a9feb50b9
thenuwah <162116832+thenuwah@users.noreply.github.com>
2024-12-05 20:20:32 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "zmRzRDzb#xi4lLyEZ1DAmnl3z58X16TvmmWkMn3ulRzmM-xgSxPM",
+MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg"
+};
